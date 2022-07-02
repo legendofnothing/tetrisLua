ce88e8ac403d155931fefda6b4ca4c603e8031d4
legendofnothing <huyrtb123@gmail.com>
2022-07-02 19:59:53 +0700

    Reorganize Files
    Figured out how to do a lua project. Basically you make modules and include then in a single script or more. basically a bunch of headers and a source file

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
 --Require Love2D Support extension.
 
+--Constant
 SCREENWIDTH, SCREENHEIGHT = 512, 416
 
---Start() Unity
+--Love 2D Callback Functions
 function love.load()
     love.window.setMode(SCREENWIDTH, SCREENHEIGHT, {fullscreen = false, centered = true, display = 1})
     love.window.setTitle("tetrisLua")
+end
+
+function love.update(dt)
+    
 end
 
 
